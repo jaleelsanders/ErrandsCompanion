12b28eb7312588579fdc0491c3d8011415482162
ffranco4 <filiberto.franco.31@gmail.com>
2021-08-03 11:50:15 -0500

    Initial Commit

---
+++ new project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new project2
+//
+//  Created by Student20 on 8/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
